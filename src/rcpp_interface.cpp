0940ead8c82afc9e68bfe371f5d0c8bb6e4698dd
Oliver Moravčík <o.moravcik@gmail.com>
2018-04-08 16:51:46 +0200

    reduced printing output

--- src/rcpp_interface.cpp
+++ src/rcpp_interface.cpp
@@ -30,7 +30,7 @@ void buildTree(NumericMatrix data, int BF_B, int BF_L, double threshold_T,
     Global::get().setDistanceMetric(distance_metric);
     Global::get().setClusterSizeMetric(cluster_size_metric);
     
-    std::cout << "jebem" << std::endl;
+    std::cout << "Building CF-Tree start" << std::endl;
 
     // build empty tree
     std::shared_ptr<CFTree> tree = Global::get().makeNewTree();
@@ -47,26 +47,27 @@ void buildTree(NumericMatrix data, int BF_B, int BF_L, double threshold_T,
         addPointToTree(tree, point);
     }
 
-    checkTheTree(tree);
+    std::cout << "Building CF-Tree end" << std::endl;
 
-    std::vector<int> nodesCounts = cauntNodesCounts(tree);
-    std::cout << "Nodes Counts control func: NLNode = " << nodesCounts[0]
-              << "; LNode = " << nodesCounts[1]
-              << "; Subclusters = " << nodesCounts[2]
-              << "; Memory = " << nodesCounts[3] << std::endl;
+//    checkTheTree(tree);
+
+//    std::vector<int> nodesCounts = cauntNodesCounts(tree);
+//    std::cout << "Nodes Counts control func: NLNode = " << nodesCounts[0]
+//              << "; LNode = " << nodesCounts[1]
+//              << "; Subclusters = " << nodesCounts[2]
+//              << "; Memory = " << nodesCounts[3] << std::endl;
 
     std::cout << "Nodes Counts mantained: NLNode = " << tree->NLNode_count
             << "; LNode = " << tree->LNode_count
             << "; Subclusters = " <<tree->subclusters << std::endl;
 
-    std::cout << "Consumed Virtual: " << memory::getConsumedVirtual() << std::endl;
-    std::cout << "Consumed Physical: " << memory::getConsumedPhysical() << std::endl;
-
     std::cout << "Total Physical: " << memory::getTotalPhysical() << std::endl;
     std::cout << "Available Physical: " << memory::getAvailiblePhysical() << std::endl;
+    std::cout << "Consumed Physical: " << memory::getConsumedPhysical() << std::endl;
+
     std::cout << "Total Virtual: " << memory::getTotalVirtual() << std::endl;
     std::cout << "Available Virtual: " << memory::getAvailibleVirtual() << std::endl;
-
+    std::cout << "Consumed Virtual: " << memory::getConsumedVirtual() << std::endl;
 
     return;
 }
